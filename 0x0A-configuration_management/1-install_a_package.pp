c991455d22e9427b2047f7706ea9d802b202dcc5
Nickson K Rop <nickyrutto@gmail.com>
2024-01-19 04:54:13 -0800

    installing a package using Puppet

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,15 @@
+# installing flask from pip3
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'werkzeug':
+  ensure   => '1.0.1',
+  provider => 'pip3',
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  require  => Package['python3-pip', 'werkzeug'],
+  provider => 'pip3',
+}
