49b4f316fb627443d7ec1147904f33ca2ceebec3
Nickson K Rop <nickyrutto@gmail.com>
2024-02-23 08:45:25 -0800

    intall flask

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# This manifest will install flask from pip3
+
+package { 'python3-pip':
+    ensure => installed,
+}
+
+exec { 'install_flask':
+    command     => '/usr/bin/pip3 install flask==2.1.0',
+    path        => ['/usr/bin'],
+    environment => [],
+    unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
