35e8669b239e5f53db3ae5560c3024e0d5888526
Nickson K Rop <nickyrutto@gmail.com>
2024-02-23 08:34:51 -0800

    Revert "remove ensure attribute"
    This reverts commit f3244b6c5be8f5275b3ff107abab1ae0df153f73.
    
    revert to ensure attribute

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 # This manifest creates a file
 file {'/tmp/school':
-    path    => '/tmp/school/'
+    ensure  => 'file',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
