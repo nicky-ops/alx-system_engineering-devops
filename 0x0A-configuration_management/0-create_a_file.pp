f3244b6c5be8f5275b3ff107abab1ae0df153f73
Nickson K Rop <nickyrutto@gmail.com>
2024-02-23 08:33:56 -0800

    remove ensure attribute

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 # This manifest creates a file
 file {'/tmp/school':
-    ensure  => 'file',
+    path    => '/tmp/school/'
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
