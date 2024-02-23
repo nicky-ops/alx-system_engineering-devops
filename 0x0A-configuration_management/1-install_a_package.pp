efc9105b0f7d28b11f2393d75fa17abbe9a34071
Nickson K Rop <nickyrutto@gmail.com>
2024-02-23 08:49:10 -0800

    install werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,7 @@ package { 'python3-pip':
 }
 
 exec { 'install_flask':
-    command     => '/usr/bin/pip3 install flask==2.1.0',
+    command     => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.1.1',
     path        => ['/usr/bin'],
     environment => [],
     unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
